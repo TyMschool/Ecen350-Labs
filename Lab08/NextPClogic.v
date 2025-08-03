00838cf2f5c10196178296e9dd149416db55b958
Paul Gratz <pgratz@gratz1.com>
2025-08-03 17:32:48 -0500

    moving files around

--- Lab08/NextPClogic.v
+++
@@ -1,9 +0,0 @@
-module NextPClogic(NextPC, CurrentPC, SignExtImm64, Branch, ALUZero, Uncondbranch); 
-   input [63:0] CurrentPC, SignExtImm64; 
-   input 	Branch, ALUZero, Uncondbranch; 
-   output [63:0] NextPC; 
-
-   /* write your code here */ 
-
-
-endmodule
